8dcf56fcaf94d1d54ccc98166471d3dbf0352319
Kuritsyn Nikolay <kuritsyn@tcinet.ru>
2021-02-21 12:31:52 +0300

    init

---
+++ tdd/javascript-2.js
@@ -0,0 +1,8 @@
+/**
+ * Возвращает уникальный селектор к элементу в DOM
+ * так что вызов document.querySelectorAll(getPath($0)).length === 1 если $0 является HTML-элементом в DOM
+ * @param {HTMLElement} node
+ */
+function getPath(node) {
+    return null;
+}
