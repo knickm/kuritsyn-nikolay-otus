97d46956b689c1bf773cb267a995f703103ee570
Kuritsyn Nikolay <kuritsyn@tcinet.ru>
2021-04-04 09:37:26 +0300

    refactoring

--- lesson-14/src/tree.ts
+++ lesson-14/src/tree.ts
@@ -1,31 +1,56 @@
 import * as fs from 'fs/promises';
 import { join, resolve } from 'node:path';
+import { exit } from 'node:process';
 
-const files: Array<string> = [];
-const dirs: Array<string> = [];
+class CTree {
+	static files: Array<string> = [];
+	static dirs: Array<string> = [];
 
-async function tree(path: string) {
-	return fs.realpath(path)
-		.then(p => fs.opendir(p))
-		.then(async d => {
-			for await (const dirent of d) {
-				const p = join(path, dirent.name);
-				if (dirent.isDirectory()) {
-					dirs.push(p);
-					await tree(p);
-				} else if (dirent.isFile()) {
-					files.push(p);
+	private async tree(path: string): Promise<{ files: Array<string>, dirs: Array<string> }> {
+		return fs.realpath(path)
+			.then(p => fs.opendir(p))
+			.then(async d => {
+				for await (const dirent of d) {
+					const p = join(path, dirent.name);
+					if (dirent.isDirectory()) {
+						CTree.dirs.push(p);
+						await this.tree(p);
+					} else if (dirent.isFile()) {
+						CTree.files.push(p);
+					}
 				}
-			}
-		})
+				return { files: CTree.files, dirs: CTree.dirs }
+			})
+	}
+
+	static main(argv: Array<string>) {
+		const argvs = argv.slice(2);
+
+		if (argvs.length === 0 || argvs[0] === '--help') {
+			console.info("tree - вывод списка файлов и папок файловой системы");
+			console.info(`
+Usage:
+node tree.js path
+
+  --help     вывод этой подсказки
+  --resolve  выведет полный путь к файлам и директории
+             иначе покажет относительные пути
+`);
+			exit(0);
+		}
+
+		const resolvePath = argvs.length === 2 && argvs[1] === '--resolve';
+
+		let path = argvs[0];
+		if (resolvePath) {
+			path = resolve(path);
+		}
+
+		new CTree().tree(path)
+			.then(r => console.log(r))
+			.catch(e => console.error("Что то пошло не так:", e));
+
+	}
 }
 
-const path = process.argv[2];
-
-if (!path || path === '--help') {
-	console.log("Usage:\nnode tree.js path");
-} else {
-	tree(resolve(path))
-		.then(() => console.log({ files, dirs }))
-		.catch(e => console.error("Что то пошло не так:", e));
-}
+CTree.main(process.argv);
